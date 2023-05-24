b3bd4e2842dabff286040842c10ffe6d420d45db
APPokhare <amrutapokhare09@gmail.com>
2023-05-24 14:37:10 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+//import MuiAlert from "@material-ui/lab/Alert";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
+
---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import axios  from 'axios'
+import NavBar from '../comps/NavBar'
+import { toast } from 'react-toastify'
+//import MuiAlert from "@material-ui/lab/Alert";
+
+export default function Home() {
+
+  const [registerUsername, setRegisterUsername] = useState('')
+  const [ registerPassword, setRegisterPassword ] = useState('')
+  const [loginUsername, setLoginUsername] = useState('')
+  const [ loginPassword, setLoginPassword ] = useState('')
+
+  const [ user, setUser ] = useState(null)
+
+  const register = () => {
+    axios({
+      method: 'post',
+      data: {
+        username: registerUsername,
+        password: registerPassword
+      },
+      withCredentials: true,
+      url: 'http://localhost:3001/register'
+    }).then(res => {console.log(res)}).catch(err => {console.log(err)})
+  }
+  const login = () => {
+    axios({
+      method: 'post',
+      data: {
+        username: loginUsername,
+        password: loginPassword
+      },
+      withCredentials: true,
+      url: 'http://localhost:3001/login'
+    }).then(res => {
+      console.log("Data:"(res.data));
+      if (res.data === "User not found") {
+      dispatch({ type: UPDATE_USER_SUCCESS});
+      // here
+      toast.success("User updated");
+      }
+  })
+      
+  }
+ // {console.log(res)}).catch(err => {console.log(err)})
+  const getUser  = () => {
+    axios({
+      method: 'get',
+      withCredentials: true,
+      url: 'http://localhost:3001/getUser'
+    }).then(res => {setUser(res.data.username)}).catch(err => {console.log(err)})
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <NavBar></NavBar>
+      <div>
+        <div>
+          <h1>Register</h1>
+          <input type="text" name="username" placeholder="username" onChange={e => setRegisterUsername(e.target.value )}/>
+          <input type="password" name="password" placeholder="password" onChange={e => setRegisterPassword(e.target.value)}/>
+          <button onClick={register}>Submit</button>
+        </div>
+
+        <div>
+          <h1>Login</h1>
+          <input type="text" name="username" placeholder="username" onChange={e => setLoginUsername(e.target.value )}/>
+          <input type="password" name="password" placeholder="password" onChange={e => setLoginPassword(e.target.value )}/>
+          <button onClick={login}>Submit</button>
+        </div>
+
+        <div>
+          <h1>Get User</h1>
+          <button onClick={getUser}>Submit</button>
+          {user ? <h1>{user}</h1> : null}
+        </div>
+      </div>
+    </div>
+  )
+}
